18169893f2b2bf888ce8313dcd457cd83a1caec0
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-08-02 12:47:13 +0300

    delete structs

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,18 @@
-package main
+package app
 
 import (
 	"github.com/valyala/fasthttp"
 	"log"
+	"restapi2/internal/service"
 	"restapi2/internal/startup"
 )
 
-func main() {
+func Run() {
+	cache := service.NewCache()
+	err := cache.Load()
+	if err != nil {
+		log.Fatal("error load cache:", err)
+	}
 	requestHandler := startup.CreateRequestHandler()
 	if err := fasthttp.ListenAndServe(":8080", requestHandler); err != nil {
 		log.Fatal(err)
--- main.go
+++ main.go
@@ -2,51 +2,9 @@ package main
 
 import (
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/valyala/fasthttp"
-	"log"
-	"restapi2/internal/handlers"
-	"restapi2/internal/service"
+	"restapi2/cmd/app"
 )
 
-type Bench struct {
-	BenchDate string
-	Catalog   string
-	Filters   string
-}
-
-type MemMetrics struct {
-	BenchDate            string
-	OffHeap              int
-	InHeap               int
-	InStack              int
-	TotalUsedMemory      int
-	AllocationRates      int
-	NumberOfLiveObjects  int
-	RateObjectsAllocated int
-	Goroutines           int
-}
-
 func main() {
-	cache := service.NewCache()
-	err := cache.Load()
-	if err != nil {
-		log.Fatal("error load cache:", err)
-	}
-	requestHandler := func(ctx *fasthttp.RequestCtx) {
-		switch string(ctx.Path()) {
-		case "/stat":
-			handlers.StatHandler(ctx, cache)
-		case "/full":
-			handlers.FullHandler(ctx, cache)
-		case "/compare":
-			handlers.CompareHandler(ctx, cache)
-		default:
-			ctx.Error("not found", fasthttp.StatusNotFound)
-		}
-	}
-
-	if err := fasthttp.ListenAndServe(":8080", requestHandler); err != nil {
-		log.Fatal(err)
-	}
-
+	app.Run()
 }
